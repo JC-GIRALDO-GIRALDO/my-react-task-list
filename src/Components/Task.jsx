14fe91fbc094348366a855d3f21e6f93f50e93c5
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-07-09 20:00:36 -0500

    codigo css

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,19 +1,20 @@
 import { FaBullseye } from "../../node_modules/react-icons/fa6";
-import { useState } from "react"
-export default function Task(){
+import { useState } from "react";
+export default function Task() {
+  const [crossOutTask, setCrossOutTask] = useState(false);
 
-    const [crossOutTask, setCrossOutTask ] = useState(false)
+  const crossOut = () => {
+    setCrossOutTask(!crossOutTask);
+  };
 
-    const crossOut = () => {
-        setCrossOutTask(!crossOutTask);
-        }
+  const buttonColor = crossOutTask ? "darkgreen" : "darkred";
 
-        const buttonColor = crossOutTask ? "darkgreen" : "darkred";
-    
-
-    return <>
-    <button style={{color: buttonColor}} onClick={crossOut}> <FaBullseye/> 
-       </button>
-                
+  return (
+    <>
+      <button style={{ color: buttonColor }} onClick={crossOut}>
+        {" "}
+        <FaBullseye />
+      </button>
     </>
-} 
+  );
+}
