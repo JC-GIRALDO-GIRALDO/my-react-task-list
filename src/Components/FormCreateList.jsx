5813a145bf39bd92f07eeddcb9f737cb0faf4933
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-08-04 21:39:35 -0500

    se puso un formulario a la lista de tareas

--- src/Components/FormCreateList.jsx
+++ src/Components/FormCreateList.jsx
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 import { FaPlusSquare } from "react-icons/fa";
 
 export default function FormCreateList({ addList }) {
-  const [values, setValues] = useState({ name: "" });
+  const [values, setValues] = useState({ name: "", description: "" });
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setValues({ ...values, [name]: value });
+    // Clear the error for the input when the user starts typing
+    setErrors({ ...errors, [name]: null });
   };
 
   const handleForm = (event) => {
     event.preventDefault();
-    if (values.name.trim() !== "") {
-      addList(values.name);
-      setValues({ name: "" });
+    const { name, description } = values;
+
+    // Validate the name length (at least 3 characters)
+    if (name.trim().length < 3) {
+      setErrors({ ...errors, name: "Name must have at least 3 characters." });
+      return;
     }
+
+    // Call addList only if the name is valid
+    addList(name, description);
+    setValues({ name: "", description: "" });
   };
 
   return (
@@ -26,9 +36,16 @@ export default function FormCreateList({ addList }) {
         placeholder="Add your new task..."
         onChange={handleInputChange}
       />
+      {errors.name && <span className="error">{errors.name}</span>}
       <button type="submit">
         <FaPlusSquare className="icons" />
       </button>
+      <textarea
+        name="description"
+        value={values.description || ""}
+        placeholder="Add task description..."
+        onChange={handleInputChange}
+      />
     </form>
   );
 }
